01f96df9452b689d6b109a54c6983963cc93587a
Rishad Baniya <54678051+rishadbaniya@users.noreply.github.com>
2023-08-15 19:31:53 +0000

    Init

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,7 +18,7 @@ import React from "react";
  */
 import image from "../images/hustle_alone_anime.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Anime picture";
 
 /**
  * Sort description that expands on your title on the Home component.
